5038706357c3e89880f8af9ef116ee7ad6c7eb28
Albert <92622147+Acorzo1983@users.noreply.github.com>
2024-11-14 12:48:35 +0100

    Delete js directory

--- js/components/connectionOptions.js
+++
@@ -1,9 +0,0 @@
-export function addConnectionOptions(command) {
-  if (document.getElementById('random-agent').checked) command += ' --random-agent';
-  if (document.getElementById('tor').checked) command += ' --tor';
-  
-  const proxy = document.getElementById('proxy').value;
-  if (proxy) command += ` --proxy="${proxy}"`;
-  
-  return command;
-}
--- js/components/crawlingOptions.js
+++
@@ -1,9 +0,0 @@
-export function addCrawlingOptions(command) {
-  const crawlDepth = document.getElementById('crawl-depth').value;
-  const crawlExclude = document.getElementById('crawl-exclude').value;
-  
-  if (crawlDepth) command += ` --crawl=${crawlDepth}`;
-  if (crawlExclude) command += ` --crawl-exclude="${crawlExclude}"`;
-  
-  return command;
-}
